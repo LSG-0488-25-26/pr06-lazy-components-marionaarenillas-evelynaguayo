b9464cf59930c992d1760294a1af4f22157130d8
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2026-01-14 22:45:33 +0100

    Data Class CatItem

---
+++ app/src/main/java/com/example/lazycomponents/model/CatItem.kt
@@ -0,0 +1,10 @@
+package com.example.lazycomponents.model
+
+// Gat de la API
+data class CatItem(
+    val id: String,
+    val titol: String,
+    val text: String,
+    val imageURL: String,
+    val tags: List<String>
+)
