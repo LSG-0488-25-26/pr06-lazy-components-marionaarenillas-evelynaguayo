a6569f12049e23c9fea1379cbaed0051e39cf7c1
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2026-01-14 23:46:49 +0100

    Corregir MainActivity

--- app/src/main/java/com/example/lazycomponents/MainActivity.kt
+++ app/src/main/java/com/example/lazycomponents/MainActivity.kt
@@ -3,45 +3,20 @@ package com.example.lazycomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lazycomponents.ui.theme.LazyComponentsTheme
+import androidx.activity.viewModels
+import com.example.lazycomponents.navigation.AppNav
+import com.example.lazycomponents.viewmodel.CatViewModel
 
 class MainActivity : ComponentActivity() {
+    // ViewModel lligat al cicle de vida de l’Activity (MVVM)
+    private val catViewModel: CatViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            LazyComponentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            // Aquí mostrem el NavHost (llista / detall)
+            AppNav(catViewModel = catViewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyComponentsTheme {
-        Greeting("Android")
     }
 }
